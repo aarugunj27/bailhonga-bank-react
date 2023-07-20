1f5c9c0b4d331e97eadfd3dd5aa1ab0b65328817
Aarush Gunjal <aarugunj27@ncs.charter.k12.de.us>
2023-07-20 14:11:33 -0400

    3d img carousel

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,44 @@ import React from "react";
 import './static/css/style.css';
 import NavBar from '../components/NavBar.js';
 import Footer from "../components/footer";
-//import { Fancybox } from "@fancyapps/ui";
-import "@fancyapps/ui/dist/fancybox/fancybox.css";
+import { Carousel } from "react-carousel3";
+import Image1 from "./static/images/InMemory_SSMoogi.png";
+import Image2 from "./static/images/Sahitya_Aaladkatti_IAS.png";
+import Image3 from "./static/images/MahantShivyogi.png";
+import Image4 from "./static/images/BankPresidents.png";
+import Image5 from "./static/images/BoardMemebers.png";
+
+const imgstyles = {
+    width: 341.1486486,
+    height: 340,
+};
 
 function Home() {
     return (
         <div>
-            <NavBar/>
+            <NavBar />
             <title>Home</title>
             <h1>Home</h1>
-            <Footer/>
+            <div style={{ display: 'flex', justifyContent: 'center'}}>
+                <Carousel height={500} width={980} yOrigin={42} yRadius={48} autoPlay={true}>
+                    <div key={1}>
+                        <img alt="" style={imgstyles} src={Image1} />
+                    </div>
+                    <div key={2}>
+                        <img alt="" style={imgstyles} src={Image2} />
+                    </div>
+                    <div key={3}>
+                        <img alt="" style={imgstyles} src={Image3} />
+                    </div>
+                    <div key={4}>
+                        <img alt="" style={imgstyles} src={Image4} />
+                    </div>
+                    <div key={5}>
+                        <img alt="" style={imgstyles} src={Image5} />
+                    </div>
+                </Carousel>
+            </div>
+            <Footer />
         </div>
     )
 }
